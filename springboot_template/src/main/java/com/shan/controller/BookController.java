d7d52fb64f4b5f5a2fa29b994a855dc00cabaa2c
Yishan566 <royce_zhang@163.com>
2022-05-10 14:46:50 +0800

    first attempt

---
+++ springboot_template/src/main/java/com/shan/controller/BookController.java
@@ -0,0 +1,14 @@
+package com.shan.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    @GetMapping
+    public String getBooks() {
+        return "hello Books";
+    }
+}
